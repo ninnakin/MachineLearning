6c5a2a05ccff8f383b1851b49d5a7d8fdfbb70b9
ninnakin <kristinajuhlin@gmail.com>
2015-11-14 16:44:22 +0100

    finalized prediction modelling

--- Quiz2.R
+++ Quiz2.R
@@ -26,26 +26,41 @@ testing = adData[-inTrain,]
 
 IL <- training[,grep("^IL",names(training))]
 pc <- preProcess(IL, method="pca", thresh=0.80)
+pc
+summary(prcomp(IL))
+# Why so different results? 7 vs 3 variables
 
 # Q4
 # Create a training data set consisting of only the predictors with variable names beginning with 
 # IL and the diagnosis. Build two predictive models, one using the predictors as they are and one
 # using PCA with principal components explaining 80% of the variance in the predictors. Use method="glm" 
 # in the train function. What is the accuracy of each method in the test set? Which is more accurate?
+set.seed(3433)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
 
-IL$diagnosis <- diagnosis[ inTrain];
+IL <- training[,grep("^IL",names(training))]
 
+# set variance explained to 80%
 ctrl <- trainControl(preProcOptions = list(thresh = 0.80))
-pca<-train(IL[,-13], IL$diagnosis, method="glm", preProcess = "pca", trControl = ctrl)
-non_pca<-train(IL[,-13], IL$diagnosis, method="glm")
+pca<-train(IL,training$diagnosis, method="glm", preProcess = "pca", trControl = ctrl)
+non_pca<-train(IL, training$diagnosis, method="glm")
 
 IL_test <- testing[,grep("^IL",names(testing))]
-IL_test$prediction = "";
 
-p<-predict(non_pca, IL_test)
+p_non<-predict(non_pca, IL_test)
+p_pca<-predict(pca, IL_test)
 
-predict(non_pca, IL_test, preProcess=pca)
+# Compute accuracy
+mean(testing$diagnosis==p_non)
+mean(testing$diagnosis==p_pca)
 
 
-
-
+# Can method return accuraccy by itself?
+IL_test$prediction <- testing$diagnosis
+p_non<-predict(non_pca, IL_test)
+p_pca<-predict(pca, IL_test)
+mean(testing$diagnosis==p_non)
+mean(testing$diagnosis==p_pca)
+# Yes :)
